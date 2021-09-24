d1bddad702bbd2bbcb437cc40cbf716b445381e3
Ruijun (Regina) Ni <75625953+reginanee@users.noreply.github.com>
2021-09-23 20:10:39 -0500

    Add files via upload

---
+++ hw2/app.js
@@ -0,0 +1,39 @@
+document.getElementById("blegen").addEventListener("mouseover",function(){
+  document.getElementById("gopher").src="img/blegen.jpg";
+});
+
+document.getElementById("willey").addEventListener("mouseover",function(){
+  document.getElementById("gopher").src="img/willey.jpg";
+});
+
+document.getElementById("walter").addEventListener("mouseover",function(){
+  document.getElementById("gopher").src="img/walter.jpg";
+});
+
+document.getElementById("keller1").addEventListener("mouseover",function(){
+  document.getElementById("gopher").src="img/keller.jpg";
+});
+
+document.getElementById("coffman").addEventListener("mouseover",function(){
+  document.getElementById("gopher").src="img/coffman.jpg";
+});
+
+document.getElementById("keller2").addEventListener("mouseover",function(){
+  document.getElementById("gopher").src="img/keller.jpg";
+});
+
+document.getElementById("svf").addEventListener("mouseover",function(){
+  document.getElementById("gopher").src="img/svf.jpg";
+});
+
+document.getElementById("tate").addEventListener("mouseover",function(){
+  document.getElementById("gopher").src="img/tate.jpg";
+});
+
+document.getElementById("teahouse").addEventListener("mouseover",function(){
+  document.getElementById("gopher").src="img/teahouse.jpg";
+});
+
+document.getElementById("rec").addEventListener("mouseover",function(){
+  document.getElementById("gopher").src="img/rec.jpg";
+});
